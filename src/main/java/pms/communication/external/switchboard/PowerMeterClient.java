6a9099c1b8ddb18599108a2de91aac96913561e2
Ki-jeong Kang <dev.kjkang@gmail.com>
2023-07-24 19:17:59 +0900

    #전력 계전기(계통 측) 클라이언트 추가 #전력 계측기(EV 충전기 측) 클라이언트 추가

---
+++ src/main/java/pms/communication/external/switchboard/PowerMeterClient.java
@@ -0,0 +1,11 @@
+package pms.communication.external.switchboard;
+
+/**
+ * Power Meter Client
+ * <p>
+ * - 전력 계측기 통신 클라이언트
+ * <p>
+ * - EV 충전기 측의 전력 계측기
+ */
+public class PowerMeterClient {
+}
---
+++ src/main/java/pms/communication/external/switchboard/PowerRelayClient.java
@@ -0,0 +1,11 @@
+package pms.communication.external.switchboard;
+
+/**
+ * Power Relay Client
+ * <p>
+ * - 전력 계전기 통신 클라이언트
+ * <p>
+ * - 계통(Grid) 측의 전력 계전
+ */
+public class PowerRelayClient {
+}
